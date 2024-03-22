938a42d9e9aa3f315582b97269da0f5f8e9ebd6d
Estefany-wu-chen <141693670+Estefany-wu-chen@users.noreply.github.com>
2024-03-21 18:16:28 -0700

    Changes for HW19
    Changes for HW19

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$("#View Project").on("click",function(){
+    console.log("You clicked on #View Project")
+});
+$("#target").on("click",function(){
+    alert("Project 1 is being presented");
+}); 
