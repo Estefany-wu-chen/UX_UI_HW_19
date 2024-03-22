3e99e381b2dcb78973138769c70659c8163f9ac7
Estefany-wu-chen <141693670+Estefany-wu-chen@users.noreply.github.com>
2024-03-21 19:52:30 -0700

    jQuery Changes
    Linking jQuery to prep for coding interactions

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
 $("#View Project").on("click",function(){
     console.log("You clicked on #View Project")
 });
-$("#target").on("click",function(){
-    alert("Project 1 is being presented");
-}); 
